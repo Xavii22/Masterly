8dd743193ba59e78c08c2afc1d3717bcb44e0513
Xavi <xaviercarneplandolit@gmail.com>
2023-03-05 23:49:18 +0100

    Migrations, factories and seeders of product and categories created
    Also created the migration of the many to many relationship between Category and Product models.
    
    Everything implemented except the CategoryFactory and CategorySeeder.

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product; 
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->delete();
+        Product::factory(20)->create();
+    }
+}
