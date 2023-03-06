71d89ba0f0719f9c8fb32a16664dc42311d97fd1
Xavi <xaviercarneplandolit@gmail.com>
2023-03-06 20:32:34 +0100

    Changed index view name to home
    Changed view name from index to home. Also changed references in web.php file and HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('home.index');
+    public function home() {
+        return view('pages.home');
     }
 }
