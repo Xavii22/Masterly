cc0ebadc2e38a869a798828d4e893eae4c2b0b7f
Xavi <xaviercarneplandolit@gmail.com>
2023-03-09 15:56:04 +0100

    Migrations, factories and seeders update
    Update on category and product

--- database/migrations/2023_03_05_195230_create_category_products_table.php
+++ database/migrations/2023_03_05_195230_create_category_products_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('category_product', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            
 
             $table->unsignedBigInteger('category_id')->index();
             $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
 
             $table->unsignedBigInteger('product_id')->index();
             $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
+
+            $table->timestamps();
         });
     }
 
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Category; 
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->delete();
+        Category::factory(10)->create();
     }
 }
