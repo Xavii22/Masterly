f92035ccc050d1db9dec0b9cfc2d6e9348df4818
Oscar Alonso <alonso.camenforte.oscar@alumnat.copernic.cat>
2023-03-13 19:12:16 +0100

    HTML Structure product page (wip)

--- src/resources/views/pages/product.blade.php
+++ src/resources/views/pages/product.blade.php
@@ -7,14 +7,17 @@
         @include('layouts.header')
         <div class="product">
             <section class="image">
-                <img src="" alt="">
+                <img src="https://i.ebayimg.com/images/g/9nkAAMXQI5tRiccx/s-l500.jpg" alt="">
             </section>
             <section class="content">
-                <h1>jjj</h1>
-                <h2></h2>
-                <h3></h3>
+                <h1 class="content__title">Gaviola de juguete para niños</h1>
+                <h2 class="content__category">Categoria - Subcategoria</h2>
+                <h3 class="content__price">50€</h3>
             </section>
-            <section class="description"></section>
+            <section class="details">
+                <h2 class="details__title"></h2>
+                <h2 class="details__description"></h2>
+            </section>
         </div>
         @include('layouts.footer')
     </div>
