93cda6c906f5ed7cf284ffa5a672fcf223e89f3c
Xavi <xaviercarneplandolit@gmail.com>
2023-03-21 20:34:17 +0100

    Product name and image taken from unsplash api

--- src/database/factories/ProductFactory.php
+++ src/database/factories/ProductFactory.php
@@ -3,12 +3,31 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use GuzzleHttp\Client;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
 class ProductFactory extends Factory
 {
+
+    public function getDataFromUnsplashApi() {
+        $description = null;
+
+        while($description == null || $description > 30) {
+            $client = new Client();
+            $response = $client->get('https://api.unsplash.com/photos/random?client_id=T1QPdcgkjhmCWibl_FZfkj4JhrmmK6qwNdrLHEShMGc&query=product');
+            $json = (string) $response->getBody();
+            $data = json_decode($json, true);
+            $description = $data['description'];
+        }
+
+        $image = $data['urls']['regular'];
+        $productData = array($description, $image);
+
+        return $productData;
+    }
+
     /**
      * Define the model's default state.
      *
@@ -16,53 +35,14 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $productData = $this->getDataFromUnsplashApi();
+
         return [
-            'name' => fake()->word(),
+            'name' => $productData[0],
             'description' => fake()->paragraph(10),
             'price' => fake()->numberBetween(10, 100),
             'category' => fake()->word(),
-            'image' => fake()->imageUrl(rand(480, 640), rand(480, 640), 'product')
-            //'image' => ProductFactory::getRandomImage()
+            'image' => $productData[1]
         ];
-    }
-
-    public function getRandomImage()
-    {
-        $access_key = 'T1QPdcgkjhmCWibl_FZfkj4JhrmmK6qwNdrLHEShMGc';
-        $endpoint = 'https://api.unsplash.com/photos/random';
-
-        $params = [
-            'count' => 1
-        ];
-
-        $headers = [
-            'Authorization: Client-ID ' . $access_key
-        ];
-
-        $url = $endpoint . '?' . http_build_query($params);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/cacert.pem'); // Set the path to the CA certificate bundle
-
-        $response = curl_exec($ch);
-
-        if ($response === false) {
-            echo 'Error: ' . curl_error($ch);
-        } else {
-            $data = json_decode($response, true);
-
-            if (is_array($data) && !empty($data)) {
-                $photo_url = $data[0]['urls']['regular'];
-
-                echo '<img src="' . $photo_url . '" />';
-            } else {
-                echo 'Error: Invalid response from API';
-            }
-        }
-
-        curl_close($ch);
     }
 }
