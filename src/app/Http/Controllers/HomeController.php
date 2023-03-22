b996273964083d0a2800437941696b23f69d3213
Xavi <xaviercarneplandolit@gmail.com>
2023-03-22 21:22:18 +0100

    Search method usage unsetted

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -11,16 +11,31 @@ class HomeController extends Controller
 {
     public function home(Request $request)
     {
-        $products = Product::paginate(40);
+        $query = $request->input('query');
 
+        $sort = $request->input('sort', 'recent');
+        $sortOrder = $this->setSortProducts($sort, true);
+        $sortBy = $this->setSortProducts($sort, false);
+
+        $category = $request->input('category');
         $parentCategories = Category::where('type', 'P')->get();
+        //dd($parentCategories[0]->table);
+        //$subCategory = '';
 
-        $query = $request->input('query', '');
-        $sort = '';
-        $category = '';
-        $productAmount = Product::all()->count();
+        if ($query !== '' || $query != null) {
+            $products = Product::where('name', 'like', '%' . $query . '%')
+                ->orderBy($sortBy, $sortOrder)
+                ->paginate(env('PAGINATE_NUMBER'));
+        } else {
+            $products = Product::orderBy($sortBy, $sortOrder)
+                ->paginate(env('PAGINATE_NUMBER'))
+                ->withQueryString();
+        }
 
-        return view('pages.home', compact('products', 'productAmount', 'parentCategories', 'query', 'sort', 'category'));
+        $productAmount = $products->total();
+
+        return view('pages.home', compact('products', 'productAmount', 'query', 'category', 'parentCategories', 'sort'));
+        //return view('pages.home', compact('products', 'productAmount', 'parentCategories', 'query', 'sort', 'category', 'subCategory'));
     }
 
     public function search(Request $request, $sort = 'asc')
@@ -32,8 +47,45 @@ class HomeController extends Controller
         $parentCategories = Category::where('type', 'P')->get();
         $childCategories = '';
 
+        $sortOrder = $this->setSortProducts($sort, true);
+        $sortBy = $this->setSortProducts($sort, false);
+
+        if ($category != null) {
+            $products = Product::whereHas('categories', function ($query) use ($category) {
+                $query->where('categories.parent_id', $category);
+            })
+                ->orderBy($sortBy, $sortOrder)
+                ->paginate(40)
+                ->withQueryString();
+            $categoryName = Category::where('id', $category)->value('name');
+            $subCategoryName = '';
+            if ($products->total() >= 0) {
+                $products = Product::whereHas('categories', function ($query) use ($category) {
+                    $query->where('categories.id', $category);
+                })
+                    ->orderBy($sortBy, $sortOrder)
+                    ->paginate(40)
+                    ->withQueryString();
+                $auxiliar = Category::where('id', $category)->value('parent_id');
+                $categoryName = Category::where('id', $auxiliar)->value('name');
+                $subCategoryName = Category::where('id', $category)->value('name');
+            }
+            $childCategories = Category::where('type', 'C')->where('parent_id', $category)->get();
+        } else {
+            $products = Product::where('name', 'like', '%' . $query . '%')
+                ->orderBy($sortBy, $sortOrder)
+                ->paginate(40);
+        }
+
+        $productAmount = $products->total();
+
+        return view('pages.home', compact('products', 'productAmount', 'parentCategories', 'childCategories', 'category', 'categoryName', 'subCategoryName', 'query', 'sort'));
+    }
+
+    public function setSortProducts($sort, $order)
+    {
         $sortBy = '';
-        $sortOrder = 'asc';
+        $sortOrder = '';
 
         switch ($sort) {
             case "recent":
@@ -50,46 +102,24 @@ class HomeController extends Controller
                 break;
             case "priceAsc":
                 $sortBy = 'price';
-                $sortOrder = 'asc';
+                $sortOrder = 'desc';
                 break;
             case "priceDesc":
                 $sortBy = 'price';
-                $sortOrder = 'desc';
+                $sortOrder = 'asc';
                 break;
             default:
-                echo "sort mal";
+                echo "mal";
+        }
+        if ($order) {
+            return $sortOrder;
         }
 
-        if ($category != null) {
-            $products = Product::whereHas('categories', function ($query) use ($category) {
-                $query->where('categories.parent_id', $category);
-            })
-            ->orderBy($sortBy, $sortOrder)
-            ->paginate(40)
-            ->withQueryString();
-            $categoryName = Category::where('id', $category)->value('name');
-            $subCategoryName = '';
-            if ($products->total() <= 0) {
-                $products = Product::whereHas('categories', function ($query) use ($category) {
-                    $query->where('categories.id', $category);
-                })
-                ->orderBy($sortBy, $sortOrder)
-                ->paginate(40)
-                ->withQueryString();
-                $auxiliar = Category::where('id', $category)->value('parent_id');
-                $categoryName = Category::where('id', $auxiliar)->value('name');
-                $subCategoryName = Category::where('id', $category)->value('name');
-            }
-            $childCategories = Category::where('type', 'C')->where('parent_id', $category)->get();
-        } else {
-            $products = Product::where('name', 'like', '%' . $query . '%')
-            ->orderBy($sortBy, $sortOrder)
-            ->paginate(40);
-        }
-        
-        $productAmount = $products->total();
+        return $sortBy;
+    }
 
-        return view('pages.home', compact('products', 'productAmount', 'parentCategories', 'childCategories', 'category', 'categoryName', /*'subCategoryName',*/ 'query', 'sort'));
+    private function getSubcategoriesNames()
+    {
     }
 
     public function showProduct($id)
--- src/resources/views/pages/home.blade.php
+++ src/resources/views/pages/home.blade.php
@@ -6,21 +6,52 @@
     <div class="parent">
         @include('layouts.header')
         <div class="home">
-            <section class="categories">
-                <div class="categories__title">
-                    <h2>Categoria</h2>
+            <aside class="categories">
+                <div>
+                    @if ($category != null)
+                        <a href="{{ route('pages.home') }}">hola</a>
+                        <span>/</span>
+                        <a>{{ $categoryName }}</a>
+                    @endif
                 </div>
-            </section>
+                <div class="categories__content">
+                    <div class="categories__title">
+                        <h2>Categorias</h2>
+                    </div>
+                    <div class="categories__list">
+                        {{-- @if ($category != null) --}}
+                        @foreach ($parentCategories as $parentCategory)
+                            <a
+                                href="{{ route('pages.home', ['query' => $query, 'sort' => 'recent', 'category' => $category]) }}">{{ $parentCategory->name }}</a>
+                        @endforeach
+                        {{-- @else --}}
+
+                        {{-- @endif --}}
+
+                    </div>
+                </div>
+
+            </aside>
             <section class="products-parent">
                 <div class="products-title">
-                    <h1>Artículos</h1>
+                    @if ($category != null)
+                        <h1>{{ $categoryName }}</h1>
+                    @else
+                        <h1>Artículos</h1>
+                    @endif
                 </div>
-				<div class="sort">
-                    <a class="sort__link button button--transparent" href="{{ route('search', ['query' => $query, 'sort' => 'asc']) }}">Más reciente</a>
-                    <a class="sort__link button button--transparent" href="{{ route('search', ['query' => $query, 'sort' => 'asc']) }}">Nombre (A-Z)</a>
-                    <a class="sort__link button button--transparent" href="{{ route('search', ['query' => $query, 'sort' => 'desc']) }}">Nombre (Z-A)</a>
-                    <a class="sort__link button button--transparent" href="{{ route('search', ['query' => $query, 'sort' => 'desc']) }}">Precio más alto</a>
-                    <a class="sort__link button button--transparent" href="{{ route('search', ['query' => $query, 'sort' => 'desc']) }}">Precio más bajo</a>
+                <div class="sort">
+                    <a class="sort__link button button--transparent"
+                        href="{{ route('pages.home', ['query' => $query, 'sort' => 'recent']) }}">Más reciente</a>
+                    <a class="sort__link button button--transparent"
+                        href="{{ route('pages.home', ['query' => $query, 'sort' => 'nameAsc']) }}">Nombre (A-Z)</a>
+                    <a class="sort__link button button--transparent"
+                        href="{{ route('pages.home', ['query' => $query, 'sort' => 'nameDesc']) }}">Nombre (Z-A)</a>
+                    <a class="sort__link button button--transparent"
+                        href="{{ route('pages.home', ['query' => $query, 'sort' => 'priceAsc']) }}">Precio más alto</a>
+                    <a class="sort__link button button--transparent"
+                        href="{{ route('pages.home', ['query' => $query, 'sort' => 'priceDesc']) }}">Precio más bajo</a>
+                    <p>{{ $productAmount }}</p>
                 </div>
                 <div class="products">
                     @foreach ($products as $product)
