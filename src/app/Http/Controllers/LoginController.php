e8b1214a7c050c17d29830df2b364e3ad3a8ee63
SamyTheLight <samythelight@gmail.com>
2023-03-23 16:55:23 +0100

    Created controllers
    Created LoginController & RegisterController

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login()
-    {
+    public function showLoginForm() {
         return view('pages.login');
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) { 
+            $request->session()->regenerate(); 
+
+            return redirect()->intended(route('pages.home')); 
+        }
+
+        session()->flash('status','Incorrect username or password!');
+
+        return redirect(route('login'));
+    }
+
+    public function logout(Request $request) {
+        session()->flush(); 
+        Auth::logout(); 
+
+        return redirect('login');
     }
 }
---
+++ src/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm() {
+        return view('pages.register');
+    }
+
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        session()->flash('status', "User {$data['name']} created with success!");
+        return redirect()->route('pages.login');
+    }
+}
