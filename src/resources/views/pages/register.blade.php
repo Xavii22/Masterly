090dc001c1195250ec3b3f3af2f1238168a6f413
SamyTheLight <samythelight@gmail.com>
2023-03-23 19:06:45 +0100

    Modified views login & register input classes

--- src/resources/views/pages/register.blade.php
+++ src/resources/views/pages/register.blade.php
@@ -5,34 +5,34 @@
 @section('title', 'Register page')
 
 @section('content')
-    <form class="w3-container w3-card-4" action="{{ route('register') }}" method="post">
+    <form class="form" action="{{ route('register') }}" method="post">
         @csrf
-        <h2>Register User</h2>
+        <h1 class="title">Regístrate</h1>
 
-        <p><input class="w3-input w3-border {{ $errors->has('name') ? 'w3-border-red' : '' }}" type="text" name="name"
+        <p><input class="input-login-register {{ $errors->has('name') ? 'w3-border-red' : '' }}" type="text" name="name" placeholder="Nombre de usuario"
                 required autofocus value="{{ old('name') }}">
             @if ($errors->has('name'))
                 <span class="w3-text-red"><strong>{{ $errors->first('name') }}</strong></span>
             @endif
         </p>
 
-        <p><input class="w3-input w3-border {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email"
+        <p><input class="input-login-register {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email" placeholder="Correo electrónico"
                 required autofocus value="{{ old('email') }}">
             @if ($errors->has('email'))
                 <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
             @endif
         </p>
 
-        <p><input class="w3-input w3-border {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password"
+        <p><input class="input-login-register {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password" placeholder="Contraseña"
                 name="password" required value="">
             @if ($errors->has('password'))
                 <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
             @endif
         </p>
 
-        <p><input class="w3-input w3-border" type="password" name="password_confirmation" required value=""></p>
+        <p><input class="input-login-register type="password" name="password_confirmation" required value="" placeholder="Confirmar contraseña"></p>
 
-        <p><input class="w3-button w3-black" type="submit" value="Register!"></p>
+        <p><input class="button button--blue" type="submit" value="Register!"></p>
 
         <p>¿Ya tienes una cuenta en nuestra página? <a href="{{ route('pages.login') }}">¡Inicia sesió!</a></p> 
     </form>
