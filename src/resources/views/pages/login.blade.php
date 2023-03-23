4ad2f4ba7dc5551825697a04a6f2aea641c7aa0e
SamyTheLight <samythelight@gmail.com>
2023-03-23 16:58:10 +0100

    Created views
    Created templates login.blade.php & register.blade.php

--- src/resources/views/pages/login.blade.php
+++ src/resources/views/pages/login.blade.php
@@ -1,17 +1,35 @@
+
+
 @extends('layouts.app')
 
-@section('title', 'Login')
+@section('title', 'Login page')
 
-@section('content')
-    <form class="formulario">
-        <h1>Login</h1>
-        <div class="contenedor">
-            <div class="input-contenedor">
-                <input type="text" placeholder="Correo electrónico">
-            </div>
-            <div class="input-contenedor">
-                <input type="text" placeholder="Contraseña">
-            </div>
-        </div>
+@section('content')     
+    <form class="w3-container w3-card-4" action="{{ route('login') }}" method="post">
+        @csrf
+        <h2>Inicia sesión</h2>
+
+        <p><input class="input-container {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email" placeholder="Correo electrónico"
+                required autofocus value="{{ old('email') }}">
+            @if ($errors->has('email'))
+                <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+        </p>
+
+        <p><input class="input-container {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password" placeholder="Contraseña"
+                name="password" required autofocus value="">
+            @if ($errors->has('password'))
+                <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
+            @endif
+        </p>
+        <!--
+        <label>Remember me</label>
+        <p><input class="w3-check" type="checkbox" name="remember" value="{{ old('remember') ? 'checked' : '' }}">
+        </p>
+        -->
+        <p><input class="w3-button w3-black" type="submit" value="Login!"></p>
+        <p>¿Aún no te has registrado en nuestra página?  <a class="register-link" href="{{ route('pages.register') }}">¡Regístrate!</a></p>
     </form>
+    
 @endsection
+
--- src/resources/views/pages/register.blade.php
+++ src/resources/views/pages/register.blade.php
@@ -1,16 +1,40 @@
+
+
 @extends('layouts.app')
 
-@section('title', 'Register')
+@section('title', 'Register page')
 
 @section('content')
-    <form class="formulario">
-        <h1>Registrate</h1>
-        <div class="contenedor">
-            <div class="input-contenedor">
-                <img class="item" src="{{ asset('images/account.png') }}" alt="User">
-                <input type="text" placeholder="Nombre de usuario">
-            </div>
-        </div>
+    <form class="w3-container w3-card-4" action="{{ route('register') }}" method="post">
+        @csrf
+        <h2>Register User</h2>
 
+        <p><input class="w3-input w3-border {{ $errors->has('name') ? 'w3-border-red' : '' }}" type="text" name="name"
+                required autofocus value="{{ old('name') }}">
+            @if ($errors->has('name'))
+                <span class="w3-text-red"><strong>{{ $errors->first('name') }}</strong></span>
+            @endif
+        </p>
+
+        <p><input class="w3-input w3-border {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email"
+                required autofocus value="{{ old('email') }}">
+            @if ($errors->has('email'))
+                <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+        </p>
+
+        <p><input class="w3-input w3-border {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password"
+                name="password" required value="">
+            @if ($errors->has('password'))
+                <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
+            @endif
+        </p>
+
+        <p><input class="w3-input w3-border" type="password" name="password_confirmation" required value=""></p>
+
+        <p><input class="w3-button w3-black" type="submit" value="Register!"></p>
+
+        <p>¿Ya tienes una cuenta en nuestra página? <a href="{{ route('pages.login') }}">¡Inicia sesió!</a></p> 
     </form>
 @endsection
+
