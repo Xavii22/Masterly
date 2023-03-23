ba8d5ea2c4d7e9c3f056baef81f3ff9684ef8ef3
SamyTheLight <samythelight@gmail.com>
2023-03-23 16:56:58 +0100

    Created routes to controllers
    Modified web.php adding routes to LoginController and RegisterController

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 
 /*
@@ -26,13 +27,18 @@ Route::get('/home', [HomeController::class, 'home'])->name('pages.home');
 
 Route::get('/product/{id}', [HomeController::class, 'showProduct'])->name('pages.product');
 
-Route::get('/register', [LoginController::class, 'register'])->name('pages.register');
-
 Route::get('/search', [HomeController::class, 'search'])->name('search');
-
-Route::get('/login', [LoginController::class, 'login'])->name('pages.login');
 
 Route::get('/cart', [CartController::class, 'cart'])->name('pages.cart');
 
 Route::post('/cart', [CartController::class, 'queryProducts'])->name('pages.cart');
 
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('pages.login');
+Route::get('/logout',[LoginController::class, 'logout'])->name('pages.logout');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('pages.register');
+
+Route::middleware('auth')->group(function () {
+
+});
