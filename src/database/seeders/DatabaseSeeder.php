40521597dfe48b6632bb34425c5a1d447bad9f11
Xavi <xaviercarneplandolit@gmail.com>
2023-03-30 18:24:17 +0200

    Now all the three seeders execute with the same command

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            CategoryProductSeeder::class
         ]);
     }
 }
