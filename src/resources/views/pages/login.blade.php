814cfea51670d803f3aded6a3ce049e339473e3b
Oscar Alonso <alonso.camenforte.oscar@alumnat.copernic.cat>
2023-05-20 22:15:36 +0200

    CSS Login & Register

--- src/resources/views/pages/login.blade.php
+++ src/resources/views/pages/login.blade.php
@@ -2,34 +2,26 @@
 
 @section('title', 'Login page')
 
-@section('content')     
+@section('content')
+    @include('layouts.header')
     <form class="form-login-register" action="{{ route('login') }}" method="post">
         @csrf
         <h1 class="title-login-register">Inicia sesión</h1>
 
-        <p><input class="input-login-register {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email" placeholder="Correo electrónico"
-                required autofocus value="{{ old('email') }}">
-            @if ($errors->has('email'))
-                <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
-            @endif
-        </p>
+        <input class="input-login-register {{ $errors->has('email') ? 'w3-border-red' : '' }}" type="text" name="email"
+            placeholder="Correo electrónico" required autofocus value="{{ old('email') }}">
+        @if ($errors->has('email'))
+            <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
+        @endif
 
-        <p><input class="input-login-register {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password" placeholder="Contraseña"
-                name="password" required autofocus value="">
-            @if ($errors->has('password'))
-                <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
-            @endif
-        </p>
-        <!--
-        <label>Remember me</label>
-        <p><input class="w3-check" type="checkbox" name="remember" value="{{ old('remember') ? 'checked' : '' }}">
-        </p>
-        -->
-        <p><input class="button button--blue" type="submit" value="Login!"></p>
-        <a class="forgot-password-link" href="{{ route('pages.register') }}">¿Has olvidado la contraseña?</a>
-        <p>¿Aún no te has registrado en nuestra página?  <a class="login-register-link" href="{{ route('pages.register') }}">¡Regístrate!</a></p>
+        <input class="input-login-register {{ $errors->has('password') ? 'w3-border-red' : '' }}" type="password"
+            placeholder="Contraseña" name="password" required autofocus value="">
+        @if ($errors->has('password'))
+            <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
+        @endif
+        <input class="button-login-register button button--blue" type="submit" value="Entrar">
+        <a class="login-register-link" href="{{ route('pages.register') }}">¿Has olvidado la contraseña?</a>
+        <p>¿Aún no te has registrado en nuestra página? <a class="login-register-link"
+                href="{{ route('pages.register') }}">¡Regístrate!</a></p>
     </form>
-    
-    <script src="{{ asset('js/loginLocalStorage.js') }}"></script>
 @endsection
-
