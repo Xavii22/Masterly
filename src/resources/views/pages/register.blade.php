67d65310984322b71cf40b2c49008e88f6663287
Oscar Alonso <alonso.camenforte.oscar@alumnat.copernic.cat>
2023-05-21 23:32:04 +0200

    Header register

--- src/resources/views/pages/register.blade.php
+++ src/resources/views/pages/register.blade.php
@@ -3,40 +3,41 @@
 @section('title', 'Register page')
 
 @section('content')
+    @include('layouts.header')
     <div class="form-login-register">
 
 
         <h1 class="title-login-register">Regístrate</h1>
 
         <input
-                class="input-login-register textfield textfield--loginRegister  {{ $errors->has('name') ? 'w3-border-red' : '' }}"
-                type="text" id="name" placeholder="Nombre de usuario" required autofocus value="{{ old('name') }}">
-            @if ($errors->has('name'))
-                <span class="w3-text-red"><strong>{{ $errors->first('name') }}</strong></span>
-            @endif
-        
+            class="input-login-register textfield textfield--loginRegister  {{ $errors->has('name') ? 'w3-border-red' : '' }}"
+            type="text" id="name" placeholder="Nombre de usuario" required autofocus value="{{ old('name') }}">
+        @if ($errors->has('name'))
+            <span class="w3-text-red"><strong>{{ $errors->first('name') }}</strong></span>
+        @endif
+
 
         <input
-                class="input-login-register textfield textfield--loginRegister  {{ $errors->has('email') ? 'w3-border-red' : '' }}"
-                type="text" id="email" placeholder="Correo electrónico" required autofocus
-                value="{{ old('email') }}">
-            @if ($errors->has('email'))
-                <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
-            @endif
-        
+            class="input-login-register textfield textfield--loginRegister  {{ $errors->has('email') ? 'w3-border-red' : '' }}"
+            type="text" id="email" placeholder="Correo electrónico" required autofocus value="{{ old('email') }}">
+        @if ($errors->has('email'))
+            <span class="w3-text-red"><strong>{{ $errors->first('email') }}</strong></span>
+        @endif
+
 
         <input
-                class="input-login-register textfield textfield--loginRegister  {{ $errors->has('password') ? 'w3-border-red' : '' }}"
-                type="password" placeholder="Contraseña" id="password" required value="">
-            @if ($errors->has('password'))
-                <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
-            @endif
-        
+            class="input-login-register textfield textfield--loginRegister  {{ $errors->has('password') ? 'w3-border-red' : '' }}"
+            type="password" placeholder="Contraseña" id="password" required value="">
+        @if ($errors->has('password'))
+            <span class="w3-text-red"><strong>{{ $errors->first('password') }}</strong></span>
+        @endif
+
 
         <input class="input-login-register textfield textfield--loginRegister" type="password" id="password_confirmation"
-                required value="" placeholder="Confirmar contraseña">
+            required value="" placeholder="Confirmar contraseña">
 
-        <a href="{{ route('pages.login') }}"><input class="button-login-register button button--blue" type="submit" value="Register!"></a>
+        <a href="{{ route('pages.login') }}"><input class="button-login-register button button--blue" type="submit"
+                value="Register!"></a>
 
         <p>¿Ya tienes una cuenta en nuestra página? <a class="login-register-link" href="{{ route('pages.login') }}">¡Inicia
                 sesió!</a></p>
